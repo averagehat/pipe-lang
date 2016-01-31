0b98074d50b16dc5ce2bf1ff0d988db180dafc1e
michaelpanciera <michael.panciera.work@gmail.com>
2016-01-31 00:29:03 -0500

    fixing compiler errors

--- src/main/scala/Algebra.scala
+++ src/main/scala/Algebra.scala
@@ -16,49 +16,54 @@ package object types {
   type Inputs = List[Target]
   type Outputs = List[Target]
 }
+
 import types._
 // an effect has a way of determining that it has existed or not.
 // and it has some kind of name, I suppose.
 trait Target {
-  import OS.getStateDeps
+  import OS._
 
-  def getStartFiles: Maybe[List[FileTarget]] = this match {
+  def getStartFiles: List[FileTarget] = this match {
     case RegexTarget(re) => lsRegex(re).map(FileTarget(_))
     //case GlobTarget(s)  => lsGlob(s).map(FileTarget(_))
-    case FileTarget(s)  => if exists Just(tgt) else Nothing
+    case ft @ FileTarget(s)  => if (satisfied) List(ft) else List()
   }
 
   def satisfied: Boolean = this match {
     case FileTarget(fn) => Files.exists(Paths.get(fn))
     case OrderTarget(name) => getStateDeps(name).getOrElse(false)
-    case other => getStartFiles.flatMap(_.nonEmpty).getOrElse(false)
+    //case other => getStartFiles.flatMap(_.nonEmpty).getOrElse(false)
+    case other => getStartFiles.nonEmpty
   }
   def isSubset(other: Target): Boolean = (this, other) match {
     case (FileTarget(fn1), FileTarget(fn2)) => fn1 == fn2
     case (FileTarget(fn1), RegexTarget(re)) => doesMatch(re, fn1)
-    case (RegexTarget(r1), RegexTarget(r2)) => isRegexSubset(r1, r2)
+    case (RegexTarget(r1), RegexTarget(r2)) => r1 isSubsetOf r2
     case (FileTarget(_), RegexTarget(_))    => false
   }
 }
 
-case class Task(is: Inputs, os: Outputs, script: Script) {
+case class Task(inputs: Inputs, outputs: Outputs, script: Script) {
+
   /** need to take all the inputs that match the input targets
-    (they have been pre-grouped via hopeful recursion),
-    transform them to their new concrete names, decide if they exist.
+    * (they have been pre-grouped via hopeful recursion),
+    * transform them to their new concrete names, decide if they exist.
     **/
-  def shouldRun(inputs: Inputs): Boolean =  throw new NotImlementedError("")//os.any(!_.satisfied)
+  def shouldRun(inputs: Inputs): Boolean =  throw new NotImplementedError("")//os.any(!_.satisfied)
                                            
 
 
-  def runIO: Unit = script.run(is, os) //implementations could go in ATask trait and use pattern matching
-  def runDry: Unit = println(script.populate(is, os))
+  def runIO: Unit = script.run(inputs, outputs) //implementations could go in ATask trait and use pattern matching
+  def runDry: Unit = println(script.populate(inputs, outputs))
 }
 
  object Target {
-   def filesOnly(xs: List[Target]): List[FileTarget] = xs.filter(
-     {case FileTarget(_) => true
-     case _ => false} )
-   def fileNames(xs: List[FileTarget]) = filesOnly(xs).map(_.name)
+//   def filesOnly(xs: List[Target]): List[FileTarget] = xs.filter(
+//     {case FileTarget(_) => true
+//     case _ => false} )
+   def fileNames(xs: List[Target]) = xs.filter(
+     {case FileTarget(_) => true; case _ => false} )
+        .map({case FileTarget(name) => name})
  }
 case class FileTarget(name: String) extends Target
 case class OrderTarget(name: String) extends Target
@@ -109,7 +114,7 @@ case class PythonScript(content: String) extends Script
   // *.good.fastq *.bad.fastq *.good.fastq *.bad.fastq <- *_R1_*.fastq *_R2_*.fastq
 //  make sure to ignore orderDependencies
 // but support this by splitting # outputs into groups, R1 -> good,bad, R2 -> good,bad
-// split by diving number of globs on left by number of globs/regexes on right.
+// split by diving number of globs on left by number of globss/regexes on right.
 // *.out <- noglob.in is then sort of illegal or ambiguous.
 // could even enforce this in the grammar.
 // but *.out <- ~.*.txt is legal.
@@ -119,21 +124,10 @@ case class PythonScript(content: String) extends Script
 object Graph {
   import OS._
 
-  def mapWhile(xs: List[A])(mapFn: A => A, predicate: A => Boolean) = {
-    def mapWhileIter(xs: List[A], result: List[A]): List[A] = xs match {
-      case Nil => result
-      case _ => {
-        val mapped = mapFn(xs.head)
-        if (predicate(mapped)) mapWhileIter(xs.tail, mapped :: result)
-        else result
-      }
-    }
-    mapWhileIter(xs, List())
-  }
-  def genConcreteTasks(tasks: List[Task]) =
+  def genConcreteTasks(tasks: List[Task]) = {
     // the below doesn't actually work. We need to generate all the concrete inputs 
     val (completedTasks, todoTasks) = tasks.span(t => t.inputs.forall(_.satisfied))
-
+  }
 
     /** produces a function which, given the runtime input of a filename, produces
       * a filename resulting from applying the target glob pattern.
@@ -144,18 +138,20 @@ object Graph {
         val outTemplate = "$1" + o.dropWhile (_!= '.')
         s.replaceFirst(matchGroup, outTemplate)
       }
-  val equalSort = topologicalSort((_.outputs.contains _).flip)
-  val noramalSort = topologicalSort((tgt, task) => (tasks.outputs.any(tgt isSubset _)))
+  val equalSort = topologicalSort((x,y) => y.outputs.contains(x))
+  val noramalSort = topologicalSort((tgt, task) => (task.outputs.any(tgt.isSubset(_))))
 //    val withRegexSort = topologicalSort(
 //      (tgt, task) => (task.outputs.contains(tgt) ||
 //                        tgt.outputs.any(isRegexSubset(tgt, _))))
 
-  /** May return the empty list **/
-  def topologicalSort(key: (Target, Task) => Boolean)( tasks: List[Task], tgt: Target): List[Task] = {
-    job = tasks.find( key tgt _)
-    job.flatMap( j => 
-      j :: j.inputs.map(topologicalSort(key)(tasks, _)) .filter(_.nonEmpty))
-      .getOrElse([])
+  /** May return t empty list **/
+  def topologicalSort(key: (Target, Task) => Boolean)( tasks: IList[Task], tgt: Target): IList[Task] = {
+
+    val job = tasks.find(key(tgt, _))
+    job.map( j =>
+     j ::  j.inputs.toIList.map(topologicalSort(key)(tasks, _))
+            .filter(_.nonEmpty).flatten)
+      .getOrElse(IList[Task]())
   }
 
 } // Graph object
